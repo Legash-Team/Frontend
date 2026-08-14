009ea4d0a5ee16a831f02734e6b3bc2b27c50170
Hanan Seid <hananseidmohammedh@gmail.com>
2026-08-14 17:32:05 +0300

    Revert "Created the Landing page"

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import LandingPage from '@/pages/LandingPage';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
@@ -8,7 +7,6 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/terms" element={<TermsPage />} />
